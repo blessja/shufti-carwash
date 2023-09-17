f0e1dc0f28ac31faf204c7ebfdf99d427afa64d6
Blessing Jackson <jaydenj772@gmail.com>
2023-09-17 15:34:33 +0200

    login changed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,6 @@
-import { useState, useEffect } from 'react';
-
-import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { login, reset } from '../features/auth/authSlice';
 import Spinner from '../components/Spinner';
 
 function Login() {
@@ -15,25 +12,9 @@ function Login() {
   const { phone, password } = formData;
 
   const navigate = useNavigate();
-  const dispatch = useDispatch();
   const { carWashId } = useParams(); // Get the car wash ID from the URL
 
-  const { user, isLoading, isError, isSuccess, message } = useSelector(
-    (state) => state.auth
-  );
-
-  useEffect(() => {
-    if (isError) {
-      toast.error(message);
-    }
-
-    if (isSuccess || user) {
-      const userId = user?.id || ''; // Ensure user.id is defined or set it to an empty string
-      navigate(`/${carWashId}/user/dashboard/${userId}`); // Include the car wash ID in the URL
-    }
-
-    dispatch(reset());
-  }, [user, isError, isSuccess, message, navigate, dispatch, carWashId]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -42,15 +23,39 @@ function Login() {
     }));
   };
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
 
     const userData = {
       phone,
       password,
     };
 
-    dispatch(login(userData));
+    try {
+      const response = await fetch(`https://shufti-carwash-server.vercel.app/api/users/${carWashId}/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+      }
+
+      setIsLoading(false);
+      toast.success('Login successful');
+
+      // Redirect or perform any other action upon successful login
+      const userId = ''; // Replace with the user ID if applicable
+      navigate(`/${carWashId}/user/dashboard/${userId}`);
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+    }
   };
 
   if (isLoading) {
@@ -59,7 +64,7 @@ function Login() {
 
   return (
     <>
-     <section className='heading'>
+        <section className='heading'>
         <h1 style={{fontSize: "23px", fontWeight: "600", paddingTop: "20px"}} className='login-title'>
         LOG IN
         </h1>
@@ -105,3 +110,113 @@ function Login() {
 }
 
 export default Login;
+
+
+
+// import { useState, useEffect } from 'react';
+
+// import { useSelector, useDispatch } from 'react-redux';
+// import { useNavigate, useParams } from 'react-router-dom';
+// import { toast } from 'react-toastify';
+// import { login, reset } from '../features/auth/authSlice';
+// import Spinner from '../components/Spinner';
+
+// function Login() {
+//   const [formData, setFormData] = useState({
+//     phone: '',
+//     password: '',
+//   });
+
+//   const { phone, password } = formData;
+
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch();
+//   const { carWashId } = useParams(); // Get the car wash ID from the URL
+
+//   const { user, isLoading, isError, isSuccess, message } = useSelector(
+//     (state) => state.auth
+//   );
+
+//   useEffect(() => {
+//     if (isError) {
+//       toast.error(message);
+//     }
+
+//     if (isSuccess || user) {
+//       const userId = user?.id || ''; // Ensure user.id is defined or set it to an empty string
+//       navigate(`/${carWashId}/user/dashboard/${userId}`); // Include the car wash ID in the URL
+//     }
+
+//     dispatch(reset());
+//   }, [user, isError, isSuccess, message, navigate, dispatch, carWashId]);
+
+//   const onChange = (e) => {
+//     setFormData((prevState) => ({
+//       ...prevState,
+//       [e.target.name]: e.target.value,
+//     }));
+//   };
+
+//   const onSubmit = (e) => {
+//     e.preventDefault();
+
+//     const userData = {
+//       phone,
+//       password,
+//     };
+
+//     dispatch(login(userData));
+//   };
+
+//   if (isLoading) {
+//     return <Spinner />;
+//   }
+
+//   return (
+//     <>
+//      <section className='heading'>
+//         <h1 style={{fontSize: "23px", fontWeight: "600", paddingTop: "20px"}} className='login-title'>
+//         LOG IN
+//         </h1>
+       
+//       </section>
+
+//       <section className='form'>
+//         <form onSubmit={onSubmit}>
+//           <div className='form-group'>
+//             <input
+//               type='phone'
+//               className='form-control'
+//               id='phone'
+//               name='phone'
+//               value={phone}
+//               placeholder='Cell Number' 
+//               onChange={onChange}
+//               autoComplete='on'
+//             />
+//           </div>
+//           <div className='form-group'>
+//             <input
+//               type='password'
+//               className='form-control'
+//               id='password'
+//               name='password'
+//               value={password}
+//               placeholder='Password'
+//               onChange={onChange}
+//               autoComplete='on'
+//             />
+//           </div>
+
+//           <div className='form-group'>
+//             <button type='submit' className='btn btn-block'>
+//               LOG IN
+//             </button>
+//           </div>
+//         </form>
+//       </section>
+//     </>
+//   );
+// }
+
+// export default Login;
