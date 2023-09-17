78670789a4dc41fe9620c2ae02b8804b99f2a060
Blessing Jackson <jaydenj772@gmail.com>
2023-09-17 13:52:23 +0200

    server api updated

--- frontend/src/pages/UserDashboard.jsx
+++ frontend/src/pages/UserDashboard.jsx
@@ -34,7 +34,7 @@ function UserDashboard() {
   
   const fetchUserData = async (userId) => {
     try {
-      const response = await fetch(`http://localhost:5000/api/users/${userId}`);
+      const response = await fetch(`https://shufti-carwash-server.vercel.app/api/users/${userId}`);
       const data = await response.json();
 
       // console.log('Response:', data);
