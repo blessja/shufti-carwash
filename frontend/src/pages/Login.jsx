0c93690bab507ac2910efe16aa8cc9376e77fc73
Blessing Jackson <jaydenj772@gmail.com>
2023-09-20 20:21:43 +0200

    login changed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,20 +1,21 @@
-import { useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { toast } from 'react-toastify';
-import Spinner from '../components/Spinner';
+import { useNavigate, useLocation, useParams } from 'react-router-dom';
 
-function Login() {
+const Login = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const userId = location.pathname.split('/').pop(); // Extract the car wash ID from the URL
+  const { carWashId } = useParams(); // Get the car wash ID from the URL
+
+
   const [formData, setFormData] = useState({
     phone: '',
     password: '',
   });
 
   const { phone, password } = formData;
-
-  const navigate = useNavigate();
-  const { carWashId } = useParams(); // Get the car wash ID from the URL
-
-  const [isLoading, setIsLoading] = useState(false);
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -25,87 +26,62 @@ function Login() {
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    setIsLoading(true);
-  
-    const userData = {
-      phone,
-      password,
-    };
-  
+
     try {
-      const response = await fetch(`https://shufti-carwash-server.vercel.app/api/users/${carWashId}/login`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userData),
+      const response = await axios.post(`https://shufti-carwash-server.vercel.app/api/users/${carWashId}/login`, {
+        phone,
+        password,
       });
-  
-      if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.message);
-      }
-  
-      const responseData = await response.json(); // Parse the response JSON
-  
-      setIsLoading(false);
-      toast.success('Login successful');
-  
-      // Extract the user ID from the response data
-      const userId = responseData._id || ''; // Assuming the user ID is stored in the "_id" field
-  
-      // Redirect the user to the dashboard with the extracted user ID
-      navigate(`/user/dashboard/${userId}`);
+
+      // Process the login response, e.g., store the token
+      console.log('User login successful:', response.data);
+
+      // Redirect to the staff dashboard or any other desired page
+           navigate(`/user/dashboard/${userId}`);
+
     } catch (error) {
-      setIsLoading(false);
-      console.error(error); // Log the error for debugging
-      toast.error('Login failed. Please check your credentials.');
+      console.error('Error logging in as staff:', error);
+      toast.error('Login failed');
     }
   };
-  
-
-  if (isLoading) {
-    return <Spinner />;
-  }
 
   return (
     <>
-        <section className='heading'>
-        <h1 style={{fontSize: "23px", fontWeight: "600", paddingTop: "20px"}} className='login-title'>
-        LOG IN
+      <section className="heading">
+        <h1 style={{ fontSize: '23px', fontWeight: '600', paddingTop: '20px' }} className="login-title">
+          LOG IN
         </h1>
-       
       </section>
 
-      <section className='form'>
+      <section className="form">
         <form onSubmit={onSubmit}>
-          <div className='form-group'>
+          <div className="form-group">
             <input
-              type='phone'
-              className='form-control'
-              id='phone'
-              name='phone'
+              type="phone"
+              className="form-control"
+              id="phone"
+              name="phone"
               value={phone}
-              placeholder='Cell Number' 
+              placeholder="Cell Number"
               onChange={onChange}
-              autoComplete='on'
+              autoComplete="yes"
             />
           </div>
-          <div className='form-group'>
+          <div className="form-group">
             <input
-              type='password'
-              className='form-control'
-              id='password'
-              name='password'
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
               value={password}
-              placeholder='Password'
+              placeholder="Password"
               onChange={onChange}
-              autoComplete='on'
+              autoComplete="yes"
             />
           </div>
 
-          <div className='form-group'>
-            <button type='submit' className='btn btn-block'>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
               LOG IN
             </button>
           </div>
@@ -113,6 +89,127 @@ function Login() {
       </section>
     </>
   );
-}
+};
 
 export default Login;
+
+
+
+// import { useState } from 'react';
+// import { useNavigate, useParams } from 'react-router-dom';
+// import { toast } from 'react-toastify';
+// import Spinner from '../components/Spinner';
+
+// function Login() {
+//   const [formData, setFormData] = useState({
+//     phone: '',
+//     password: '',
+//   });
+
+//   const { phone, password } = formData;
+
+//   const navigate = useNavigate();
+//   const { carWashId } = useParams(); // Get the car wash ID from the URL
+
+//   const [isLoading, setIsLoading] = useState(false);
+
+//   const onChange = (e) => {
+//     setFormData((prevState) => ({
+//       ...prevState,
+//       [e.target.name]: e.target.value,
+//     }));
+//   };
+
+//   const onSubmit = async (e) => {
+//     e.preventDefault();
+//     setIsLoading(true);
+  
+//     const userData = {
+//       phone,
+//       password,
+//     };
+  
+//     try {
+//       const response = await fetch(`https://shufti-carwash-server.vercel.app/api/users/${carWashId}/login`, {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(userData),
+//       });
+  
+//       if (!response.ok) {
+//         const errorData = await response.json();
+//         throw new Error(errorData.message);
+//       }
+  
+//       const responseData = await response.json(); // Parse the response JSON
+  
+//       setIsLoading(false);
+//       toast.success('Login successful');
+  
+//       // Extract the user ID from the response data
+//       const userId = responseData._id || ''; // Assuming the user ID is stored in the "_id" field
+  
+//       // Redirect the user to the dashboard with the extracted user ID
+//       navigate(`/user/dashboard/${userId}`);
+//     } catch (error) {
+//       setIsLoading(false);
+//       console.error(error); // Log the error for debugging
+//       toast.error('Login failed. Please check your credentials.');
+//     }
+//   };
+  
+
+//   if (isLoading) {
+//     return <Spinner />;
+//   }
+
+//   return (
+//     <>
+//         <section className='heading'>
+//         <h1 style={{fontSize: "23px", fontWeight: "600", paddingTop: "20px"}} className='login-title'>
+//         LOG IN
+//         </h1>
+       
+//       </section>
+
+//       <section className='form'>
+//         <form onSubmit={onSubmit}>
+//           <div className='form-group'>
+//             <input
+//               type='phone'
+//               className='form-control'
+//               id='phone'
+//               name='phone'
+//               value={phone}
+//               placeholder='Cell Number' 
+//               onChange={onChange}
+//               autoComplete='on'
+//             />
+//           </div>
+//           <div className='form-group'>
+//             <input
+//               type='password'
+//               className='form-control'
+//               id='password'
+//               name='password'
+//               value={password}
+//               placeholder='Password'
+//               onChange={onChange}
+//               autoComplete='on'
+//             />
+//           </div>
+
+//           <div className='form-group'>
+//             <button type='submit' className='btn btn-block'>
+//               LOG IN
+//             </button>
+//           </div>
+//         </form>
+//       </section>
+//     </>
+//   );
+// }
+
+// export default Login;
