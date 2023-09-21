345a150012a86ce3d1b8cb6d189fa528b89e072f
Blessing Jackson <jaydenj772@gmail.com>
2023-09-21 17:44:34 +0200

    user

--- frontend/src/pages/UserDashboard.jsx
+++ frontend/src/pages/UserDashboard.jsx
@@ -1,17 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Grid from '@material-ui/core/Grid';
+
 
 function UserDashboard() {
   const [washHistory, setWashHistory] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [userInfo, setUserInfo] = useState(null);
-
   
   const { userId } = useParams();
 console.log('User ID:', userId);
@@ -48,76 +41,31 @@ console.log('User ID:', userId);
       });
   };
   
-  const formatDate = (dateString) => {
-    const options = { weekday: 'short', day: 'numeric', month: 'short', year: 'numeric' };
-    return new Date(dateString).toLocaleDateString(undefined, options);
-  };
-
-  const getNextWashNumber = () => {
-    const lastWashNumber = washHistory.length;
-    return lastWashNumber + 1;
-  
-  };
-
-  console.log(getNextWashNumber);
-
-
-  const onLogout = () => {
-   
-    navigate(`/${carWashId}/dashboard`);
-
-  };
-
 
   return (
-    <Container maxWidth="sm">
-
-
-      {userInfo ? (
-        <div>
-
-          <Typography variant="h5" gutterBottom style={{ color: '#4682B4', marginTop: '30px', marginBottom: '60px' }}>
-            UPCOMING DISCOUNTS
-          </Typography>
-
-
-          {washHistory.length > 0 ? (
-            <List>
-              {washHistory.map((wash, index) => (
-                <ListItem key={wash._id} style={{ color: '#12F329' }} >
-                  <Grid container alignItems="center" spacing={0}>
-                    <Grid item xs={6}>
-                      <ListItemText primary={formatDate(wash.date)} />
-                    </Grid>
-                    <Grid item xs={2}>
-                      <div style={{ position: 'relative', display: 'inline-block', backgroundColor: '#12F329', borderRadius: '50%', width: '40px', height: '40px', }}>
-                        <div style={{ position: 'absolute', top: '50%', left: '50%', backgroundColor: '#12F329', transform: 'translate(-50%, -50%)' }}>
-                          <span style={{ fontWeight: 'bold', backgroundColor: '#12F329', color: 'white' }}>{index + 1}</span>
-                        </div>
-                      </div>
-                    </Grid>
-                    <Grid item xs={4}>
-                      <ListItemText primary="WASHED" align="right" />
-                    </Grid>
-                  </Grid>
-                </ListItem>
-              ))}
-            </List>
-          ) : (
-            <Typography variant="body1">No wash history available.</Typography>
-          )}
-        
-        </div>
+    <div>
+      <h2>User Dashboard</h2>
+      {isLoading ? (
+        <p>Loading wash history...</p>
       ) : (
-        <Typography variant="body1">{error ? error : 'Loading user information...'}</Typography>
+        <div>
+          <h3>Wash History</h3>
+          {washHistory.length > 0 ? (
+            <ul>
+              {washHistory.map((wash, index) => (
+                <li key={index}>
+                  <p>Date: {wash.date}</p>
+                  <p>Washed: {wash.washed}</p>
+                  {/* Add more details as needed */}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No wash history available.</p>
+          )}
+        </div>
       )}
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '30px',  }}>
-        <button className="btn btn-block" onClick={onLogout}>
-          EXIT
-        </button>
-      </div>
-
-    </Container>
+    </div>
   );
 }
 
@@ -136,7 +84,7 @@ export default UserDashboard;
 
 // function UserDashboard() {
 //   const { userId, carWashId } = useParams();
-  // const [userInfo, setUserInfo] = useState(null);
+//   const [userInfo, setUserInfo] = useState(null);
 //   const [washHistory, setWashHistory] = useState([]);
 //   const [error, setError] = useState(null);
 
@@ -191,11 +139,11 @@ export default UserDashboard;
 
 //   console.log(getNextWashNumber);
 
-  // const onLogout = () => {
+//   const onLogout = () => {
    
-  //   navigate(`/${carWashId}/dashboard`);
+//     navigate(`/${carWashId}/dashboard`);
 
-  // };
+//   };
 
 
 //   return (
